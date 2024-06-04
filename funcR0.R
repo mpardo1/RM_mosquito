48be1f2b1251a3e0f748f018eaed7acb4aec99a7
Marta pardo araujo <45668772+mpardo1@users.noreply.github.com>
2024-06-04 18:51:28 +0200

    Update funcR0.R

--- funcR0.R
+++ funcR0.R
@@ -40,13 +40,14 @@ QuadN_func <- function(cte, c1, c2, temp){
   }
   return(outp)
 }
+
 ### Incorporating rain and human density:
 h_f <- function(hum, rain){
   # Constants: 
   erat = 0.5
   e0 = 1.5
   evar = 0.05
-  evar = 0.1
+  # evar = 0.1
   eopt = 8
   efac = 0.01
   edens = 0.01
@@ -63,7 +64,8 @@ TFD_f_alb <- function(temp){Briere_func(0.0488,8.02,35.65,temp)} # Fecundity
 pLA_f_alb <- function(temp){Quad_func(0.002663,6.668,38.92,temp)} # Survival probability Egg-Adult
 MDR_f_alb <- function(temp){Briere_func(0.0000638,8.6,39.66,temp)} # Mosquito Development Rate
 lf_f_alb <- function(temp){Quad_func(1.43,13.41,31.51,temp)} # Adult life span
-dE_f_alb <- function(temp){Quad_func(0.00071,1.73,40.51,temp)} # Adult life span
+dE_f_alb <- function(temp){Quad_func(0.00071,1.73,40.51,temp)} # egg development rate
+deltaE_f_alb <- function(temp){QuadN_func(0.0019328,-0.091868,1.3338874,temp)} # Egg mortality rate
 
 # R0 function by temperature:
 R0_func_alb <- function(Te, rain, hum){
@@ -76,8 +78,9 @@ R0_func_alb <- function(Te, rain, hum){
     dE <- dE_f_alb(Te)
     probla <- pLA_f_alb(Te)
     h <- h_f(hum,rain)
-    deltaE = 0.1
-    R0 <- ((f*a*deltaa)*probla*(h*dE/(h*dE+deltaE)))^(1/3)
+    deltaE = deltaE_f_alb(Te)#0.1
+    
+    R0 <- ((f*a*deltaa)*probla*((h*dE)/(h*dE+deltaE)))^(1/3)
   }
   return(R0)
 }
@@ -88,22 +91,24 @@ EFD_f_aeg <- function(temp){Briere_func(0.00856,14.58,34.61,temp)} # Fecundity
 pLA_f_aeg <- function(temp){Quad_func(0.004186,9.373,40.26,temp)} # Survival probability Egg-Adult
 MDR_f_aeg <- function(temp){Briere_func(0.0000786,11.36,39.17,temp)} # Mosquito Development Rate
 lf_f_aeg <- function(temp){Quad_func(0.148,9.16,37.73,temp)} # Adult life span
-dE_f_aeg <- function(temp){Briere_func(0.0003775 ,14.88,37.42,temp)} # Adult life span
+dE_f_aeg <- function(temp){Briere_func(0.0003775 ,14.88,37.42,temp)} # Egg development rate
+deltaE_f_aeg <- function(temp){QuadN_func(0.004475,-0.210787,2.552370,temp)} # Egg mortality rate
 
 # R0 function by temperature:
 R0_func_aeg <- function(Te, rain,hum){
   if(is.na(Te) | is.na(rain) | is.na(hum)){
     R0 <- NA
   }else{
-    a <- a_f_aeg(Te)
-    f <- 40
+    a <- 1#a_f_aeg(Te)
+    f <- EFD_f_aeg(Te) #40
     deltaa <- lf_f_aeg(Te)
     dE <- dE_f_aeg(Te)
     probla <- pLA_f_aeg(Te)
     h <- h_f(hum,rain)
-    deltE = 0.1
-    R0 <- ((f*a*deltaa)*probla*(h*dE/(h*dE+deltE)))^(1/3)
+    deltaE = deltaE_f_aeg(Te)
+    R0 <- ((f*a*deltaa)*probla*((h*dE)/(h*dE+deltaE)))^(1/3)
   }
   return(R0)
 }
+
 
